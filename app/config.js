1f812e0bbb553446093fdc9c515c69c18987605f
AL FAJRI PUTRA PRATAMA <72371119+fajriputra@users.noreply.github.com>
2021-07-22 09:02:59 +0700

    update database connect
    - tambah env database
    - ganti connection database

--- app/config.js
+++ app/config.js
@@ -9,6 +9,4 @@ module.exports = {
   dbName: process.env.DB_NAME,
   dbUser: process.env.DB_USER,
   dbPass: process.env.DB_PASS,
-  // dbHost: process.env.DB_HOST,
-  // dbPort: process.env.DB_PORT,
 };
